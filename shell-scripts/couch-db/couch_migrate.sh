5fd22c0491eb35656e16bbcd1583a8977e28d891
Martin Ndegwa <ndegwamartin@gmail.com>
2018-02-16 16:16:58 +0300

    Refactor script to support variable HTTB Verbs /Rest Methods

--- shell-scripts/couch-db/couch_migrate.sh
+++ shell-scripts/couch-db/couch_migrate.sh
@@ -8,24 +8,27 @@ PASSWORD=0
 CONFIG_FOLDER=.
 count="$#"
 PROTO=0
+VERB=POST
 
 
 if [ "$1" == "--help" ]
   then
-     echo -e "Specify (Optional) a database name like \033[33m./setup_view_configs.sh -d <database name> \033[0m"
-     echo -e "Specify (Optional) a host like \033[33m ./setup_view_configs.sh -h <hostname>\033[0m"
-     echo -e "Specify (Optional) a port name like \033[33m ./setup_view_configs.sh -p <port>\033[0m"
-     echo -e "Specify (Optional) a source folder like \033[33m ./setup_view_configs.sh -f <folder path>\033[0m"
-     echo -e "Specify (Optional) a username like \033[33m ./setup_view_configs.sh -u <username>\033[0m"
-     echo -e "Specify (Optional) a password like \033[33m ./setup_view_configs.sh -pwd <password>\033[0m"
-     echo -e "Specify (Optional) a protocol e.g. http or https like \033[33m ./setup_view_configs.sh -proto <protocol>\033[0m"
-     echo -e "Specify (Optional) any multiple arguments like \033[33m ./setup_view_configs.sh -u <username> -pwd <password> -d <database name>\033[0m"
-     echo -e "For Authentication you can choose to pass the username and password as part of the host e.g. \033[33m ./setup_view_configs.sh -h http://<username>:<password>@<hostname>\033[0m"
-     echo -e "Running \033[33m./setup_view_configs.sh -help \033[0m brings this help menu"
+     echo -e "Specify (Optional) a database name like \033[33m./couch_migrate.sh -d <database name> \033[0m"
+     echo -e "Specify (Optional) a host like \033[33m ./couch_migrate.sh -h <hostname>\033[0m"
+     echo -e "Specify (Optional) a port name like \033[33m ./couch_migrate.sh -p <port>\033[0m"
+     echo -e "Specify (Optional) a source folder like \033[33m ./couch_migrate.sh -f <folder path>\033[0m"
+     echo -e "Specify (Optional) a username like \033[33m ./couch_migrate.sh -u <username>\033[0m"
+     echo -e "Specify (Optional) a password like \033[33m ./couch_migrate.sh -pwd <password>\033[0m"
+     echo -e "Specify (Optional) a protocol e.g. http or https like \033[33m ./couch_migrate.sh -proto <protocol>\033[0m"
+     echo -e "Specify (Optional) a HTTB VERB or method e.g. PUT, POST(default) like \033[33m ./couch_migrate.sh -method <http verb>\033[0m"
+     echo -e "Specify (Optional) any multiple arguments like \033[33m ./couch_migrate.sh -u <username> -pwd <password> -d <database name>\033[0m"
+     echo -e "For Authentication you can choose to pass the username and password as part of the host e.g. \033[33m ./couch_migrate.sh -h http://<username>:<password>@<hostname>\033[0m"
+     
+     echo -e "Running \033[33m./couch_migrate.sh -help \033[0m brings this help menu"
 
 elif [ "$((count % 2))" != 0 ]
   then
-     echo -e "\033[1;31m None or Incorrect Parameters supplied. Run ./setup_view_configs.sh --help to view a list of valids options \033[0m"
+     echo -e "\033[1;31m None or Incorrect Parameters supplied. Run ./couch_migrate.sh --help to view a list of valids options \033[0m"
     exit 1
 
 else
@@ -60,6 +63,10 @@ else
 
        if [ $varPrev == "-proto" ];then
             PROTO="$var"
+       fi
+
+       if [ $varPrev == "-method" ];then
+            VERB="$var"
        fi
 
        varPrev="$var"
@@ -108,10 +115,10 @@ else
 
                         if [ $PASSWORD != 0 ]; then
 
-                             curl -H 'Content-Type: application/json' -vX POST $PROTO://$USERNAME:$PASSWORD@$HOST:$PORT/$DATABASE -d @$f
+                             curl -H 'Content-Type: application/json' -vX $VERB $PROTO://$USERNAME:$PASSWORD@$HOST:$PORT/$DATABASE -d @$f
                         else
 
-                             curl -H 'Content-Type: application/json' -vX POST $PROTO://$USERNAME@$HOST:$PORT/$DATABASE -d @$f
+                             curl -H 'Content-Type: application/json' -vX $VERB $PROTO://$USERNAME@$HOST:$PORT/$DATABASE -d @$f
                         fi
 
                     else
@@ -119,7 +126,7 @@ else
                       if [[ $PROTO == 0 ]]; then
                              PROTO=http
                       fi
-                             curl -H 'Content-Type: application/json' -vX POST $PROTO://$HOST:$PORT/$DATABASE -d @$f
+                             curl -H 'Content-Type: application/json' -vX $VERB $PROTO://$HOST:$PORT/$DATABASE -d @$f
 
 
                     fi
@@ -135,7 +142,7 @@ else
             echo -e "\033[1;32m Script run complete...\033[0m"
     else
 
-        echo -e "\033[1;31m You need to supply a database name. For help Run  ./setup_view_configs.sh --help \033[0m"
+        echo -e "\033[1;31m You need to supply a database name. For help Run  ./couch_migrate.sh --help \033[0m"
         exit 1
 
     fi
